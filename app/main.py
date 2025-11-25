a415f59919c6a145aef36b50cb26f881f710f6b9
KyleBoBa <kylebb1@gmail.com>
2025-11-25 01:23:46 +0100

    3.0

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.write(f"{input()}: command not found\n")
 
 if __name__ == "__main__":
     main()
