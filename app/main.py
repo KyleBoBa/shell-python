db10777679ded472cbf161dc73496cdf8953058b
KyleBoBa <kylebb1@gmail.com>
2025-11-25 01:57:30 +0100

    5.0

--- app/main.py
+++ app/main.py
@@ -1,14 +1,34 @@
 import sys
+from allowed_commands import ALLOWED_COMMANDS as AC
 
+
+def validate_command(command):
+    if not (command in AC):
+        sys.stdout.write(f"{command}: command not found\n")
+        return False
+    return True
+
+def exec(command, args):
+    if not validate_command(command):
+        return True
+    if command == AC[0]:
+        return False
+    elif command == AC[1]:
+        print(f"{" ".join(args)}")
+    else:
+        print(command)
+    return True
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
+        entry = input()
+        command = entry.split()[0]
+        args = entry.split()[1:]
+
+        if not exec(command, args):
             break
-        sys.stdout.write(f"{command}: command not found\n")
-        
+
 
 if __name__ == "__main__":
     main()
